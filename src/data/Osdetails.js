c86a137286302e8bd19d7658d2601f3800c5981c
Abdul Rafay <191401@students.au.edu.pk>
2022-12-19 03:16:22 +0500

    os details table setting

--- src/data/Osdetails.js
+++ src/data/Osdetails.js
@@ -6,17 +6,19 @@ function Osdetails () {
     return (
         
         <>
-<div class="mx-auto bg-grey-lightest">
-    
+    <div class="mx-auto bg-grey-lightest">
+
     <div class="min-h-screen flex flex-col">
 
         <header class="bg-nav">
+
             <div class="flex justify-between">
-                <div class="p-1 mx-3 inline-flex items-center">
-                    <i class="fas fa-bars pr-2 text-white" onclick="sidebarToggle()"></i>
+
+            <div class="p-1 mx-3 inline-flex items-center">
+            <i class="fas fa-bars pr-2 text-white" onclick="sidebarToggle()"></i>
                     <h1 class="text-white p-2">MediCare</h1>
-                </div>
-                <div class="p-1 flex flex-row items-center">
+            </div>
+            <div class="p-1 flex flex-row items-center">
                     <img onclick="profileToggle()" class="inline-block h-8 w-8 rounded-full" src="https://avatars0.githubusercontent.com/u/4323180?s=460&v=4" alt=""/>
                     <a href="#" onclick="profileToggle()" class="text-white p-2 no-underline hidden md:block lg:block">Mohammad Ali</a>
                     <div id="ProfileDropDown" class="rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
@@ -28,12 +30,14 @@ function Osdetails () {
                         </ul>
                     </div>
                 </div>
+
             </div>
+
         </header>
 
-
         <div class="flex flex-1">
-            <aside id="sidebar" class="bg-side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block">
+
+        <aside id="sidebar" class="bg-side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block">
                 <div class="flex">
 
                 </div>
@@ -51,14 +55,6 @@ function Osdetails () {
                            class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
                             <i class="fab fa-wpforms float-left mx-2"></i>
                             OS Registration
-                            <span><i class="fa fa-angle-right float-right"></i></span>
-                        </a>
-                    </li>
-                    <li class="w-full h-full py-3 px-2 border-b border-light-border">
-                        <a href="/msios"
-                           class="font-sans font-hairline hover:font-normal text-sm text-nav-item no-underline">
-                            <i class="fab fa-wpforms float-left mx-2"></i>
-                            HCW Registration
                             <span><i class="fa fa-angle-right float-right"></i></span>
                         </a>
                     </li>
@@ -81,22 +77,21 @@ function Osdetails () {
                 </ul>
 
             </aside>
-      
-            <main class="bg-white-500 flex-1 p-3 overflow-hidden"/>
 
-            
-                    <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
-                        <div class="mb-2 border-solid border-gray-300 rounded border shadow-sm w-full">
-                            <div class="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
-                                Children Data
+            <main class="bg-white-500 flex-1 p-3 overflow-hidden">
+
+            <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
+            <div class="mb-2 border-solid border-gray-300 rounded border shadow-sm w-full">
+            <div class="bg-gray-200 px-2 py-3 border-solid border-gray-200 border-b">
+                                Operating Staff Data
                             </div>
                             <div class="p-3">
-                                <table class="table-responsive w-full rounded">
+                            <table class="table-responsive w-full rounded">
                                     <thead>
                                       <tr>
                                         <th class="border w-1/4 px-4 py-2">#</th>
-                                        <th class="border w-1/6 px-4 py-2">Child Name</th>
-                                        <th class="border w-1/6 px-4 py-2">Healthcare Worker</th>
+                                        <th class="border w-1/6 px-4 py-2">Name</th>
+                                        <th class="border w-1/6 px-4 py-2">CNIC</th>
 
                                         <th class="border w-1/5 px-4 py-2">Actions</th>
                                       </tr>
@@ -105,7 +100,7 @@ function Osdetails () {
                                         <tr>
                                             <td class="border px-4 py-2">1</td>
                                             <td class="border px-4 py-2">Yaqoob</td>
-                                            <td class="border px-4 py-2">Jamshed</td>
+                                            <td class="border px-4 py-2">9999999</td>
                                             <td class="border px-4 py-2">
                                                 <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
                                                         <i class="fas fa-eye"></i></a>
@@ -119,7 +114,7 @@ function Osdetails () {
                                         <tr>
                                             <td class="border px-4 py-2">2</td>
                                             <td class="border px-4 py-2">Yaseen</td>
-                                            <td class="border px-4 py-2">Jamshed</td>
+                                            <td class="border px-4 py-2">9999999</td>
                                             <td class="border px-4 py-2">
                                                 <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
                                                         <i class="fas fa-eye"></i></a>
@@ -133,7 +128,7 @@ function Osdetails () {
                                         <tr>
                                             <td class="border px-4 py-2">3</td>
                                             <td class="border px-4 py-2">Salar</td>
-                                            <td class="border px-4 py-2">Ali</td>
+                                            <td class="border px-4 py-2">9999999</td>
                                             
                                       
                                             <td class="border px-4 py-2">
@@ -149,7 +144,7 @@ function Osdetails () {
                                         <tr>
                                             <td class="border px-4 py-2">4</td>
                                             <td class="border px-4 py-2">Saqlain</td>
-                                            <td class="border px-4 py-2">Ali</td>
+                                            <td class="border px-4 py-2">9999999</td>
                                             <td class="border px-4 py-2">
                                                 <a class="bg-teal-300 cursor-pointer rounded p-1 mx-1 text-white">
                                                         <i class="fas fa-eye"></i></a>
@@ -163,19 +158,19 @@ function Osdetails () {
                                        
                                     </tbody>
                                 </table>
-                            </div>
-                        </div>
-                    </div>
-              
-                </div>
+                                </div>
             </div>
-            
+            </div>
+
+
+                </main>
+
         </div>
        
-        <footer class="bg-grey-darkest text-white p-2">
-            <div class="flex flex-1 mx-auto">&copy; My Design</div>
-        </footer>
-        <script src="./main.js"></script>
+        </div>
+
+    </div>
+    <script src="./main.js"></script>
         </>
       )  
       
